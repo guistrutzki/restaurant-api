dfe1bd007ab0752f9803fa530dd15132d2a1b002
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-10-11 14:14:46 -0300

    feat: add auth middleware

--- src/shared/infra/http/routes/restaurants.routes.ts
+++ src/shared/infra/http/routes/restaurants.routes.ts
@@ -1,14 +1,16 @@
 import { CreateRestaurantController } from '@modules/places/useCases/createRestaurant/CreateRestaurantController';
 import { ListRestaurantsController } from '@modules/places/useCases/listRestaurants/ListRestaurantsController';
 import { Router } from 'express';
+import { ensuredAuthenticated } from 'middlewares/ensureAuthenticated';
 
 const restaurantsRoutes = Router();
 
 const createRestaurantController = new CreateRestaurantController();
 const listRestaurantController = new ListRestaurantsController();
 
-restaurantsRoutes.post('/', createRestaurantController.handle);
+restaurantsRoutes.use(ensuredAuthenticated);
 
+restaurantsRoutes.post('/', createRestaurantController.handle);
 restaurantsRoutes.get('/', listRestaurantController.handle);
 
 export { restaurantsRoutes };
