d9a59d26340092f7866fc8261a7a2b01479e5c80
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-10-14 18:39:19 -0300

    wip: remove auth middleware temporary

--- src/shared/infra/http/routes/restaurants.routes.ts
+++ src/shared/infra/http/routes/restaurants.routes.ts
@@ -1,4 +1,4 @@
-import { ensuredAuthenticated } from '@middlewares/ensureAuthenticated';
+// import { ensuredAuthenticated } from '@middlewares/ensureAuthenticated';
 import { CreateRestaurantController } from '@modules/places/useCases/createRestaurant/CreateRestaurantController';
 import { ListRestaurantsController } from '@modules/places/useCases/listRestaurants/ListRestaurantsController';
 import { Router } from 'express';
@@ -8,7 +8,7 @@ const restaurantsRoutes = Router();
 const createRestaurantController = new CreateRestaurantController();
 const listRestaurantController = new ListRestaurantsController();
 
-restaurantsRoutes.use(ensuredAuthenticated);
+// restaurantsRoutes.use(ensuredAuthenticated);
 
 restaurantsRoutes.post('/', createRestaurantController.handle);
 restaurantsRoutes.get('/', listRestaurantController.handle);
